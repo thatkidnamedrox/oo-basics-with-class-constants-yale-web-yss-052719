67696e387fd4fe21fff388ef4488ec051da0d1b4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 15:21:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  #attr_reader :brand
 
   BRANDS = []
   def initialize(brand)
@@ -12,6 +12,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-
+  def brand=(brand)
+    @brand = brand
 
 end
